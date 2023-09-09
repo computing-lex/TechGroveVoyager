8ba8f36411b4faf42d71ddc9c32a88a05c3ace8c
jrivera6000 <jrivera6000@gmail.com>
2023-09-09 19:12:25 -0400

    Player Update and Knob Interactions

---
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/rotateNob.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.MagicLeap;
+using static MagicLeapInputs;
+
+public class rotateNob : MonoBehaviour
+{
+    MagicLeapInputs mlInputs;
+    MagicLeapInputs.ControllerActions controllerActions;
+    Vector2 triggerValue;
+    Animator nob = null;
+
+    bool turnTheNob;
+
+    private void Start()
+    {
+        mlInputs = new MagicLeapInputs();
+        mlInputs.Enable();
+        controllerActions = new MagicLeapInputs.ControllerActions(mlInputs);
+        controllerActions.TouchpadPosition.performed += HandleOnTouchpad;
+        nob = GetComponent<Animator>();
+    }
+
+    private void HandleOnTouchpad(InputAction.CallbackContext obj)
+    {
+        triggerValue = obj.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        //nob.SetFloat("Horizontal", triggerValue.x);
+        //nob.SetFloat("Vertical", triggerValue.y);
+       
+        if (turnTheNob)
+        {
+            Debug.Log(triggerValue);
+            nob.SetFloat("Horizontal", triggerValue.x);
+            nob.SetFloat("Vertical", triggerValue.y);
+        }
+    }
+
+    public void allowToTurn()
+    {
+        turnTheNob = true;
+
+    }
+
+    public void stopTurning()
+    {
+        turnTheNob = false;
+    }
+}
