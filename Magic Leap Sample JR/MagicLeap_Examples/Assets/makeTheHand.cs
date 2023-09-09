002063750757281b9af0ce7a5f0f95ad905bb391
jrivera6000 <jrivera6000@gmail.com>
2023-09-09 19:52:51 -0400

    Sphere Update

--- Magic Leap Sample JR/MagicLeap_Examples/Assets/makeTheHand.cs
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/makeTheHand.cs
@@ -10,7 +10,7 @@ public class makeTheHand : MonoBehaviour
     MagicLeapInputs.ControllerActions controllerActions;
     float triggerButtonValue;
 
-    public MeshRenderer hand;
+    public GameObject hand;
 
     bool handTriggered = false;
     // Start is called before the first frame update
@@ -20,6 +20,7 @@ public class makeTheHand : MonoBehaviour
         mlInputs.Enable();
         controllerActions = new MagicLeapInputs.ControllerActions(mlInputs);
         controllerActions.TriggerButton.performed += HandleOnTrigger;
+        controllerActions.TriggerButton.canceled += HandleOnTrigger;
     }
 
     private void HandleOnTrigger(InputAction.CallbackContext obj)
@@ -30,10 +31,14 @@ public class makeTheHand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(triggerButtonValue > 0 && !handTriggered)
+        if(triggerButtonValue != 0 && !handTriggered)
         {
-            hand.enabled = true;
+            Instantiate(hand,transform.position,Quaternion.identity);
             handTriggered = true;
+        }
+        if(triggerButtonValue == 0 && handTriggered)
+        {
+            handTriggered = false;
         }
     }
 }
