2d30adf673b9ea07d1750acd19bb1b50204d61ae
Nebula <bikminpikmin@gmail.com>
2023-09-10 12:40:40 -0400

    Added Fog, Sound Basics
    lol!!!!

--- Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/CheckAtomImage.cs
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/CheckAtomImage.cs
@@ -8,14 +8,21 @@ public class CheckAtomImage : MonoBehaviour
     public Image leftImage;
     public Image rightImage;
 
+    public AudioSource goodSound;
+    public AudioSource badSound;
+    public AudioSource clickSound;
+
     public void checkAtoms()
     {
-        if(leftImage.sprite.name == "Hydrogen1" &&  rightImage.sprite.name == "Hydrogen2")
+        clickSound.PlayOneShot(clickSound.clip);
+        if (leftImage.sprite.name == "Hydrogen1" &&  rightImage.sprite.name == "Hydrogen2")
         {
+            goodSound.PlayDelayed(1);
             Debug.Log("Good");
         }
         else
         {
+            badSound.PlayDelayed(1);
             Debug.Log("Bad");
         }
     }
