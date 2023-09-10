9c626f5b5bd6d5509d7710446f62bfc7f5fc62c2
jrivera6000 <jrivera6000@gmail.com>
2023-09-10 13:17:27 -0400

    Updating Hub Machine

--- Magic Leap Sample JR/MagicLeap_Examples/Assets/FrequencySetter.cs
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/FrequencySetter.cs
@@ -7,7 +7,7 @@ public class FrequencySetter : MonoBehaviour
 
     public LineRenderer frequencyWave;
     int frequency;
-
+    bool frequencyChanged = false;
     float[] yFrequency = new float[30];
 
     // Start is called before the first frame update
@@ -19,9 +19,22 @@ public class FrequencySetter : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (!frequencyChanged)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Vector3 currentPosition = frequencyWave.GetPosition(i);
+                Vector3 newPosition = new Vector3(currentPosition.x, Random.Range(-0.05f, 0.05f), currentPosition.z);
+                frequencyWave.SetPosition(i, newPosition);
+            }
+        }
+    }
+
     public void RunTheFrequency()
     {
-        
+        frequencyChanged = true;
         if (frequency == 5)
         {
             for (int i = 0; i < 30; i++)
