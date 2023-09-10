3208c450d31b1dd474d4eeb1e65f20b85437c436
tanzdoesthings <90532166+tanzdoesthings@users.noreply.github.com>
2023-09-10 12:48:50 -0400

    update branch

--- Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/CheckAtomImage.cs
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/CheckAtomImage.cs
@@ -8,14 +8,21 @@ public class CheckAtomImage : MonoBehaviour
     public Image leftImage;
     public Image rightImage;
 
+    public AudioSource clickSound;
+    public AudioSource goodSound;
+    public AudioSource badSound;
+
     public void checkAtoms()
     {
+        clickSound.Play();
         if(leftImage.sprite.name == "Hydrogen1" &&  rightImage.sprite.name == "Hydrogen2")
         {
+            goodSound.PlayDelayed(1);
             Debug.Log("Good");
         }
         else
         {
+            badSound.PlayDelayed(1);
             Debug.Log("Bad");
         }
     }
