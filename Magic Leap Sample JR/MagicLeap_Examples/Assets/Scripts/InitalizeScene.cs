546e369fb72404596e7d862322e00535d6331a29
tanzdoesthings <90532166+tanzdoesthings@users.noreply.github.com>
2023-09-10 10:08:36 -0400

    New positioning system

--- Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/InitalizeScene.cs
+++ Magic Leap Sample JR/MagicLeap_Examples/Assets/Scripts/InitalizeScene.cs
@@ -10,10 +10,15 @@ public class initalizeScene : MonoBehaviour
     MagicLeapInputs.ControllerActions controllerActions;
     float triggerButtonValue;
 
-    public GameObject hand;
+    public GameObject reference;
     public GameObject origin;
+    public bool initalized = false;
 
-    bool handTriggered = false;
+    [SerializeField] private Vector3[] calibration = new Vector3[2];
+    private int calibrationCount = 0;
+    private GameObject[] referencePoints = new GameObject[2];
+    private bool handTriggered = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,15 +37,35 @@ public class initalizeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (triggerButtonValue != 0 && !handTriggered)
+        if (calibrationCount >= calibration.Length)
         {
-            origin.transform.position = transform.position;
-            origin.transform.rotation = new Quaternion(0, transform.rotation.y, 0, 0);
+            origin.transform.position = calibration[0];
+            origin.transform.LookAt(new Vector3(calibration[1].x, origin.transform.position.y, calibration[1].z));
+            
+            foreach (GameObject point in referencePoints)
+            {
+                Destroy(point);
+            }
+
+            calibrationCount = 0;
+            initalized = true;
+        }
+
+        if (triggerButtonValue != 0 && !handTriggered && !initalized)
+        {
+            setCalibrationPoint(transform.position, calibrationCount);
+            referencePoints[calibrationCount] = Instantiate(reference, calibration[calibrationCount], Quaternion.identity);
+            calibrationCount++;
             handTriggered = true;
         }
         if (triggerButtonValue == 0 && handTriggered)
         {
             handTriggered = false;
         }
+    }
+
+    void setCalibrationPoint(Vector3 point, int index)
+    {
+        calibration[index] = point;
     }
 }
